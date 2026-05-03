a0450a7080ab699f3e2a7daf2c7b65e23ff3e92c
rasel-69 <rasel4897981@gmail.com>
2026-05-04 01:26:00 +0600

    updated with supabase connection

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,7 +4,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "" 
 })
 
 
@@ -13,5 +13,5 @@ export const {
     signUp,
     signOut,
     useSession
-}= createAuthClient()
+}= authClient
 
